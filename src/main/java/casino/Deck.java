5111927e39dc0ec61d8c2829b71c0c444c1932d4
aboyce002 <aboyce002@my.wilmu.edu>
2019-10-16 13:25:37 -0400

    added cards and deck

---
+++ src/main/java/casino/Deck.java
@@ -0,0 +1,72 @@
+package casino;
+import java.util.*;
+
+public class Deck {
+    private int deckSize = 52;
+    private List<Card> deck = new ArrayList<Card>();
+
+    //initialize with one pack/52 cards
+    public Deck(){
+        addCards();
+    }
+
+    //option to initialize with multiple packs of cards for games like Blackjack
+    public Deck(int x) {
+        for(int i = 0; i < x; i++)
+            addCards();
+        deckSize = x * 52;
+    }
+
+    //add every type of card to the deck
+    private void addCards(){
+        for(int suit = 0; suit <= 3; suit++){
+            for(int rank = 0; rank <= 12; rank++){
+                deck.add(new Card(suit, rank));
+            }
+        }
+    }
+
+    //adds to the *top* of the deck
+    public void addTop(Card c){
+        if(deck.size() < deckSize)
+            deck.add(c);
+        else
+            System.out.println("Deck full.");
+    }
+
+    //adds to the *bottom* of the deck
+    public void addBottom(Card c){
+        if(deck.size() < deckSize)
+            deck.add(0, c);
+        else
+            System.out.println("Deck full.");
+    }
+
+    //get count
+    public int cardsLeft(){
+        return deck.size();
+    }
+
+    //use for testing
+    public void displayDeck(){
+        for(Card d : deck){
+            System.out.println(d);
+        }
+    }
+
+    public void shuffle(){
+        Collections.shuffle(deck);
+    }
+
+    public Card getCard(){
+        return deck.remove(deck.size()-1);
+    }
+
+    public int getDeckSize() {
+        return deckSize;
+    }
+
+    public void setDeckSize(int deckSize) {
+        this.deckSize = deckSize;
+    }
+}
