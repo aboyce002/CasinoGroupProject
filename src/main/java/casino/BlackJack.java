3494a262fc263816d64af6fa13c3740c3bf1c53a
ChristianNeal015 <cjdaking030@gmail.com>
2019-10-16 19:32:14 -0400

    update 1

---
+++ src/main/java/casino/BlackJack.java
@@ -0,0 +1,13 @@
+package casino;
+
+import sun.java2d.loops.DrawGlyphList;
+
+public class BlackJack {
+ private String[] userInput;
+
+ public BlackJack(String [] userInput){
+     this.userInput = userInput;
+
+ }
+
+}
